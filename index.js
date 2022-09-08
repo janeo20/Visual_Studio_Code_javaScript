0f37b500efb7e474d9cd14284ed843560bec8cb4
janeo20 <janderson-oliveira16@hotmail.com>
2022-09-08 16:15:28 -0300

    exercicio

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let nome, sobrenome, peso, altura, dataInternação, diabete
+/*let nome, sobrenome, peso, altura, dataInternação, diabete
 
 nome = prompt ("digite seu nome")
 sobrenome = prompt ("digite seu sobrenome")
@@ -12,5 +12,36 @@ alert ("seu nome é: " + nome
 + "\nseu peso é: " + peso
 + "\nsua altura é: " + altura
 + "\nsua data de internação é: " + dataInternação
-+ "\ndiabético: " + diabete)
++ "\ndiabético: " + diabete)*/
+
+/*let r, d 
+d = prompt ("digite seu valor em dólar")
+r = 5.21 * d
+alert ("seu valor em real é: " + r)*/
+
+/*let precoDeCusto, percentual, venda
+precoDeCusto = prompt ("insira o preço de custo: ")
+percentual = prompt ("insira o percentual: ")
+venda = (parseInt(precoDeCusto) + (precoDeCusto * percentual / 100))
+alert ("o valor de venda é: " + venda)*/
+
+/*let valorDepositado, valorRendido, juros
+valorDepositado = prompt ("valor depositado")
+juros = valorDepositado * 1.007
+alert ("valor rendido foi: " + juros)*/
+
+/*let idade
+idade = prompt ("digite sua idade")
+if (idade >= 80){
+    alert ("voce é mt velho")
+}else if (idade <= 25){
+    alert ("ta novo ainda")
+}else if (idade == 40){
+    alert ("ta ficando velho")
+}*/
+
+
+
+
+
 
