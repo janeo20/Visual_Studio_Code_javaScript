cae68301f24e517255082144f7ed9681df0cc4bc
janeo20 <janderson-oliveira16@hotmail.com>
2022-09-08 13:22:27 -0300

    exercicio 2 08.09.22

---
+++ index.js
@@ -0,0 +1,16 @@
+let nome, sobrenome, peso, altura, dataInternação, diabete
+
+nome = prompt ("digite seu nome")
+sobrenome = prompt ("digite seu sobrenome")
+peso = prompt ("digite seu peso")
+altura = prompt ("digite sua altura")
+dataInternação = prompt ("digite sua data de internação")
+diabete = prompt ("diabético sim ou não??")
+
+alert ("seu nome é: " + nome
++ "\nseu sobrenome é: " + sobrenome
++ "\nseu peso é: " + peso
++ "\nsua altura é: " + altura
++ "\nsua data de internação é: " + dataInternação
++ "\ndiabético: " + diabete)
+
