e623efce8ddf823510855101356cef8c0e799e7f
janeo20 <janderson-oliveira16@hotmail.com>
2022-09-21 11:14:53 -0300

    Exercicios_15_09_22

---
+++ exercicios.js
@@ -0,0 +1,186 @@
+// let num, por, resultado
+// num = prompt ("Digite um número: ")
+// por = prompt ("digite a porcentagem: " )
+
+// function abacaxi (num, por){
+//     resultado = parseInt(num) * (parseInt(por) / 100)
+//     return alert (resultado)
+// }
+
+// abacaxi (num, por)
+
+
+
+
+
+// let real, dolar, resultado
+// dolar = prompt ("digite seu valor em dólar: ")
+
+// function conversao (real, dolar){
+//     real = parseFloat(5.21) * parseFloat(dolar)
+//     resultado = real
+//     return alert (resultado)
+// }
+
+// conversao (real, dolar)
+
+
+
+
+// let F, C, resultado
+// F = prompt ("digite sua temperatura em em fahrenheit: ")
+
+// function conversao (F, C){
+//     C = (F - 32) / 1.8
+//     resultado = C
+//     return alert ("sua temperatura em celsius é: " + resultado)
+// }
+
+// conversao(F, C)
+
+
+
+
+
+// let F, C, resultado
+// C = prompt ("digite sua temperatura em em Celsius: ")
+
+// function conversao (F, C){
+//     F = (C * 1.8) + 32
+//     resultado = F
+//     return alert ("sua temperatura em fahrenheit é: " + resultado)
+// }
+
+// conversao(F, C)
+
+
+
+
+// let peso, altura, resultado;
+
+// peso = prompt ("digite seu peso: ")
+// altura = prompt ("digite sua altura: ")
+
+// function imc (peso, altura){
+//     resultado = peso / (altura * altura)
+//     return alert ("seu imc é: " + resultado)
+// }
+
+// imc (peso, altura)
+
+
+
+
+// let n1, n2, n3, maior, menor;
+
+// n1 = prompt ("digite o primeiro número: ")
+// n2 = prompt ("digite o segundo número: ")
+// n3 = prompt ("digite o terceiro número: ")
+
+// function maiorNumero (n1, n2, n3){
+//     if(n1 >= n2 && n1 >= n3){
+//         maior = n1
+//     }else if (n2 >= n1 && n2 >= n3){
+//         maior = n2
+//     }else if (n3 >= n1 && n3 >= n2){
+//         maior = n3
+//     }else{
+
+//     }
+//     return ("maior número: " + maior)
+// }
+// alert(maiorNumero(n1, n2, n3))
+
+
+//  function menorNumero (n1, n2, n3){
+//  if(n1 <= n2 && n1 <= 3){
+//     menor = n1
+//  }else if (n2 <= n1 && n2 <= n3){
+//     menor = n2
+//  }else if(n3 <= n1 && n3 <= n2){
+//     menor = n3
+//  }else{
+
+//  }
+ 
+//  return ("menor número: " + menor)
+// }
+// alert(menorNumero(n1, n2, n3))
+
+
+
+
+// let taxaImposto, custo, resultado;
+// taxaImposto = prompt ("digite o valor da porcentagem: ")
+// custo = prompt ("digite o valor de custo: ")
+
+
+
+// function somaImposto ( taxaImposto, custo){
+//     resultado = (parseInt(taxaImposto) / 100) * custo
+//     return alert ("o valor é: " + resultado)
+
+// }
+// somaImposto (taxaImposto, custo)
+
+
+
+
+// let v1, v2, resultado
+// v1 = prompt("digite o primeiro valor: ")
+// v2 = prompt ("digite o segundo valor: ") 
+
+// function tabuada(v1, v2){
+//     soma = parseInt(v1) + parseInt(v2)
+//     sub = parseInt(v1) - parseInt(v2)
+//     mult = parseInt(v1) * parseInt(v2)
+//     div = parseInt(v1) / parseInt(v2)
+    
+    
+//     return alert(soma + "\n" + sub + "\n" + mult + "\n" + div)
+// }
+
+// tabuada(v1, v2)
+
+
+
+
+// let num, resultado
+// num = prompt ("digite o número de seu mês: ")
+
+// function mes(num){
+//     if(num == 1){
+//         alert("janeiro")
+//     }else if(num == 2){
+//         alert("fevereiro")
+//     }else if(num == 3){
+//         alert("março")
+//     }else if(num == 4){
+//         alert("abril")
+//     }else if(num == 5){
+//         alert("maio")
+//     }else if(num == 6){
+//         alert("junho")
+//     }else if(num == 7){
+//         alert("julho")
+//     }else if(num == 8){
+//         alert("agosto")
+//     }else if(num == 9){
+//         alert("setembro")
+//     }else if(num == 10){
+//         alert("outubro")
+//     }else if(num == 11){
+//         alert("novembro")
+//     }else if( num == 12){
+//         alert("dezembro")
+//     }else{
+//         alert("número invalido")
+//     }
+        
+//     }
+// mes(num)
+
+
+   
+
+
