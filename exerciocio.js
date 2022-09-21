c8fd4dbe60d65d4f74b2c0a02bb85d0bf0fe48aa
janeo20 <janderson-oliveira16@hotmail.com>
2022-09-21 11:07:40 -0300

    Exercicios_13_09_22

---
+++ exerciocio.js
@@ -0,0 +1,93 @@
+                                    //exercicio 1 for
+
+
+
+// let s, f, ft = 0, st= 0, ms = 0, mc = 0.0
+// for(i = 1; i <= 20; i++){
+//     s=prompt("Digite o seu rendimento: ")
+    
+//     f=prompt("Digite quantidade de filhos: ")
+    
+//     st = st + s
+//     ft = ft + f
+//     if(s > ms){
+//         ms = s
+//     }
+//     if(s <= 100){
+//         mc++
+//     }
+// }
+// alert("O maior salário é: " + ms)
+// alert("A média de filhos é: " + ft/20)
+// alert("A média do salário é: " + st/20)
+// alert("Percentual de pessoas com salário até R$ 100: " + (mc/20)*100 + "%")
+
+
+                                             //exercicio 2
+
+// let Num = 0.0;
+
+// for(i = 0; i <= 500; i++){
+//        if(i % 2 != 0 && i % 3 == 0){
+//               alert(Num)
+//               Num = Num + 1
+//        }
+// }
+// alert ("")
+
+
+
+
+                                    // exercicio 1 while
+
+// let Soma = 0, Media = 0, Valores = 0;
+
+// while (Valores >= 0){
+//     Valores = prompt ("digite um valor: ")
+//     Soma = parseInt(Valores) + Soma
+// }
+
+// alert ("total da soma" + Soma )
+
+
+
+
+//                                   exercicio 2
+
+// let Num 
+// Num = prompt ("Digite um número: ")
+
+// while (Num < 300){
+//     alert (Num)
+//     Num = Num * 3
+// }
+
+
+
+
+//exercicio 1 Do while
+
+// let c = 233
+
+// do{
+//     if(c >= 300 && c <= 456){
+//        c += 3;
+//     }else{
+//         c += 5;
+//     }
+//     alert(c);
+//
+// }while (c <= 456)
+
+
+
+//exercicio 2 
+
+// let N, Soma = 0
+// N = prompt ("digite um numero: ")
+// do{
+    
+ 
+    
+// }while()
+
